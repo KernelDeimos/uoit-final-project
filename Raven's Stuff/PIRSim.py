5739290ce658564239b4dd65f7e0f5415ca3a923
bradyjibanez <bradyjibanez@gmail.com>
2018-11-06 10:52:26 -0500

    Update Sensor data Json representation

--- Raven's Stuff/PIRSim.py
+++ Raven's Stuff/PIRSim.py
@@ -5,18 +5,20 @@ import os
 
 occupants = 0
 temperature = 16.5
-humidity = 45
+humidity = 45.5
+count = 1
 
 while True:
     occupants = sensorSim.occupancyUp(occupants)
     occupants = sensorSim.occupancyDown(occupants)
     temperature = sensorSim.temperature(temperature)
-    Occupancy = {'sensorData': {'SensorID': "PIR1", 'Occupants': occupants}}
-    Environment = {'sensorData': {'SensorID': "TH1", 'Temperature': temperature, 'Humidity': humidity}}
-    jsonReturn1 = json.dumps(Occupancy, sort_keys=True, indent=4)
+    humidity = sensorSim.humidity(humidity)
+    Environment = {'sensorData': {'SensorID': "Sensor1", 'Temperature': temperature, 'Humidity': humidity,
+                   'Occupancy': occupants}}
     jsonReturn2 = json.dumps(Environment, sort_keys=True, indent=4)
-    print(jsonReturn1)
+    print("Sensor Reading", count)
     print(jsonReturn2)
+    count += 1
     time.sleep(2)
 
 
