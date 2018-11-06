75867d83de3ed32c23c4e69994e8bc2e055c02f6
bradyjibanez <bradyjibanez@gmail.com>
2018-11-05 22:38:12 -0500

    Including tentaive sensor reading script

---
+++ SensorCommunication/sensorInput.py
@@ -0,0 +1,37 @@
+import smbus2 #For i2c intercom from i2c out of sensors (hum and temp)
+from gpiozero import MotionSensor #provides methods for easy use PIR
+                                  #used for serial ttl signal from PIR thru
+                                  #Pi GPIO
+
+tempAddress = 0x?? #will be different when configured
+               #represents i2c addy from sensor taking in temp when read
+               #using i2cdetect -y 0
+humAddress = 0x??
+bus = smbus2.SMBus(0) #initialize i2c connection for data intake
+
+
+
+def temperature():
+    currentTemp = bus.read_byte_data(tempAddress, ?) #register param will depend
+                                                  #on where coming from on bus
+    return currentTemp
+
+def humidity():
+    currentHum = bus.read_byte_data(humAddress, ?)
+    return currentHum
+
+def occupancyUp():
+    occupantcount = 0
+    pir = MotionSensor(4)
+    if pir.motion_detected():
+        occupantcount += 1
+        return occupantcount
+
+def occupancyDown():
+    occupantcount = 0
+    pir = MotionSensor(4)
+    if pir.motion_detected():
+        occupantcount -= 1
+        return occupantcount
+
+
