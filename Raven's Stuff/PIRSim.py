1b7b25e431ff08ff5ef49be20da12248ac95f818
bradyjibanez <bradyjibanez@gmail.com>
2018-11-07 16:26:25 -0500

    Now has CBOR conversion from and back to JSON

--- Raven's Stuff/PIRSim.py
+++ Raven's Stuff/PIRSim.py
@@ -1,4 +1,4 @@
-import sensorSim, json, time, os, datetime, pytz
+import sensorSim, json, time, os, datetime, pytz, cbor
 
 occupants = 0
 temperature = 16.5
@@ -7,15 +7,21 @@ count = 1
 
 while True:
     currentTime = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    currentTimeString = currentTime.strftime('%m/%d/%y/%H/%M/%S')
     occupants = sensorSim.occupancyUp(occupants)
     occupants = sensorSim.occupancyDown(occupants)
     temperature = sensorSim.temperature(temperature)
     humidity = sensorSim.humidity(humidity)
     Environment = {'sensorData': {'SensorID': "Sensor1", 'Temperature': temperature, 'Humidity': humidity,
                    'Occupancy': occupants,'Time': currentTime}}
-    jsonReturn2 = json.dumps(Environment, sort_keys=True, indent=4, default=str)
+    jsonReturn = json.dumps(Environment, sort_keys=True, indent=4, default=str)
+    cborReturn = cbor.dumps({'sensorData': {'SensorID': 'Sensor1', 'Temperature': temperature, 'Humidity': humidity,
+                                            'Occupancy': occupants,'Time': currentTimeString}})
+    cborReceive = cbor.loads(cborReturn)
     print("Sensor Reading", count)
-    print(jsonReturn2)
+    print(jsonReturn)
+    print(cborReturn)
+    print(cborReceive) #Putts cbor back to JSON
     count += 1
     time.sleep(2)
 
