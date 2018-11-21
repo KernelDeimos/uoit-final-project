b63062bd5e52566d31a33311102848b8647ca321
bradyjibanez <bradyjibanez@gmail.com>
2018-11-21 14:35:36 -0500

    Threaded for accurate real time reading from ds1631 and DHT11

--- SensorCommunication/C->PythonImplementation/getTemperature.py
+++ SensorCommunication/C->PythonImplementation/getTemperature.py
@@ -1,28 +1,49 @@
-import signal
-import subprocess
+import signal, subprocess, threading
 from time import sleep
 import Adafruit_DHT, time
 from subprocess import call
 
 
-class TempSensor:
-	def __init__(self, executable="./temperature"):
-		self.proc = subprocess.Popen(executable, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+class TempSensors(threading.Thread):
 
 	def get_temperatures(self):
 		for line in iter(self.proc.stdout.readline, ""):
 			yield line.replace("\n", "").split(",")
 
-t = TempSensor()
+	def run(self):
+		executable="./temperature"
+		self.proc = subprocess.Popen(executable, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+		while True:
+			for index, x in enumerate(self.get_temperatures()):
+				print("Sensor1 Temperature: ", x[0], "C")
+				print("Sensor2 Temperature: ", x[1], "C")
 
-for index, x in enumerate(t.get_temperatures()):
-#	if index % 20 == 0:
-	print("Sensor1 Temperature: ", x[0], "C")
-	print("Sensor2 Temperature: ", x[1], "C")
+class HumSensor(threading.Thread):
+	def stopwatch(self, seconds):
+		start = time.time()
+		time.clock()
+		elapsed = 0
+		while elapsed <= seconds:
+			elapsed = time.time() - start
+		return elapsed
 
-	hum, temp = Adafruit_DHT.read_retry(11, 4)
-	humidity = hum
-	print('Humidity: {0:0.1f} %'.format(humidity))
-	time.sleep(1)
+	def run(self):
+		while True:
+			hum, temp = Adafruit_DHT.read_retry(11, 4)
+			humidity = hum
+			kill = self.stopwatch(5)
+			while kill != 5:
+				print('Humidity: {0:0.1f} %'.format(humidity))
+				print("From {}".format(self.getName())
+				break
+
+t = TempSensors()
+h = HumSensor()
+
+count = 0
+
+h.start()
+t.start()
+
 
 
