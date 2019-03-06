342800e21cb9bb8fd61a5de517229e85b74e4fef
bradyjibanez <bradyjibanez@gmail.com>
2019-03-05 23:04:23 -0500

    working disconnect

--- Capstone_Winter_Work/Sensor_Implementations/Wifi_Implementation/HumidityPi/Hum/Hum/getHumidity.py
+++ Capstone_Winter_Work/Sensor_Implementations/Wifi_Implementation/HumidityPi/Hum/Hum/getHumidity.py
@@ -3,33 +3,37 @@ from subprocess import call
 
 HOST = "192.168.1.40"
 port = 5454
+type = "Humidity Sensor"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, port))
-device = "HumPi connected."
 running = True
 
-class HumSensors():
+class HumSensor():
 
-	def send(self, humidity):
-		byte = humidity.encode()
-		s.sendto(byte, (HOST, port))
+        def send(self, humidity):
+                byte = humidity.encode()
+                s.sendto(byte, (HOST, port))
 
-	def run(self):
-		hum, temp = Adafruit_DHT.read_retry(11, 4)
-		humidity = "Humidity: " + str(hum) + "%"
-		print(humidity)
-		while True:
-			hum, temp = Adafruit_DHT.read_retry(11, 4)
-			check = humidity
-			humidity = "Humidity: " + str(hum) + "%"
-			if check != humidity:
-				self.send(humidity)
-				print(humidity)
-				time.sleep(1)
+        def run(self):
+                global type
+                sensType = type.encode()
+                s.send(sensType)
+                hum, temp = Adafruit_DHT.read_retry(11, 4)
+                humidity = "Humidity: " + str(hum) + "%"
+                self.send(humidity)
+                print(humidity)
+                while True:
+                        hum, temp = Adafruit_DHT.read_retry(11, 4)
+                        check = humidity
+                        humidity = "Humidity: " + str(hum) + "%"
+                        if check != humidity:
+                                self.send(humidity)
+                                print(humidity)
+                                time.sleep(1)
 
-humidity = HumSensors()
+humidity = HumSensor()
 
 try:
-	humidity.run()
+        humidity.run()
 except KeyboardInterrupt:
-	s.close()
+        s.close()
