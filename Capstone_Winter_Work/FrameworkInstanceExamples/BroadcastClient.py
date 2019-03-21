508fcdf2a1a6c6421a7965b8720c0b91f32d7ed8
bradyjibanez <bradyjibanez@gmail.com>
2019-03-21 19:35:37 -0400

    Updates to Client

--- Capstone_Winter_Work/FrameworkInstanceExamples/BroadcastClient.py
+++ Capstone_Winter_Work/FrameworkInstanceExamples/BroadcastClient.py
@@ -1,5 +1,6 @@
 import socket
 import json
+from threading import Thread
 
 #Listen for broadcasts when found return name of device found
 #Maintain map of address for devices (name tp ip associated) - dict that
@@ -19,7 +20,7 @@ def listenForBroadcast():
 			deviceAddressDict[key] = jsonData[key]
 			ip = jsonData[key]
 			print("SAVED", deviceAddressDict)
-			return ip
+			return key, ip
 
 def connectToDevice(haConnectiveRequest):
 	global connectedDeviceThreads
@@ -35,31 +36,60 @@ def connectToDevice(haConnectiveRequest):
 		print(client.recv(1024))
 		#client.close()
 	except Exception as exc:
+		client.close()
 		print(exc)
+		exit()
 	print("Connected:", connectedDeviceThreads)
 
-def sendCommand(device, command):
-	global connectedDeviceThreads
-	client = connectedDeviceThreads[device]
-	print(" ")
-	print(" ")
-	print(client)
-	client.send(str.encode(command))
-	print("Sending command %s to device %s" % (command, device))
-	response = client.recv(1024)
-	print(response)
+class SendCommandThread(Thread):
+
+	def __init__(self, device, command):
+		Thread.__init__(self)
+		self.device = device
+		self.command = command
+
+	def run(self):
+		global connectedDeviceThreads
+		device = self.device
+		command = self.command
+		client = connectedDeviceThreads[device]
+		while True:
+			try:
+				client.send(str.encode(command))
+				print("Sending command %s to device %s" % (command, device))
+				response = client.recv(1024)
+				stringResponse = str(response, "utf-8")
+				jsonResponse = json.loads(stringResponse)
+				print("**********")
+				print("%s: %s" % (jsonResponse["src"], jsonResponse["output"]))
+				print("**********")
+			except Exception as exc:
+				client.close()
+				print(exc)
+				exit()
 
 broadcastClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 broadcastClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 broadcastClient.bind(("", 37020))
 
 check = None
+
 while True:
-	device = listenForBroadcast()
+
+	deviceName, device = listenForBroadcast()
 	# TODO: make connectToDevice handle JSON instead of string
 	if check != device:
 		connectToDevice(device)
-	command = json.dumps({"src": "Hub", "code": "20", "msg": "read temp"})
-	sendCommand(device, command)
+	if deviceName == "Humidity Sensor":
+		command = "read hum"
+	elif deviceName == "Temperature Sensor":
+		command = "read temp"
+	else:
+		print(deviceName)
+		print("Fuck you you fuck stick")
+		exit()
+	command = json.dumps({"src": "Hub", "code": "20", "msg": command})
+	commandThread = SendCommandThread(device, command)
+	commandThread.start()
 	#temperature = sendCommand(command)
 	#print(temperature)
