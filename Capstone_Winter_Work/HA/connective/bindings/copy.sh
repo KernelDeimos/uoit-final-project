cfd772e11e98a1501fd75baaf7c019f9804d44d4
Eric Dubé <eric.alex.dube@gmail.com>
2019-03-23 15:15:28 -0400

    Add Python console for debugging & stats via connective

--- Capstone_Winter_Work/HA/connective/bindings/copy.sh
+++ Capstone_Winter_Work/HA/connective/bindings/copy.sh
@@ -1,15 +1,17 @@
-#::run : subdirs (store server manager devicemanager)
+#::run : subdirs (store server manager devicemanager pyconsole)
 #::end
 
 #::gen repcsv 'rm ../../$1/bindings.py' (subdirs)
 rm ../../server/bindings.py
 rm ../../manager/bindings.py
 rm ../../devicemanager/bindings.py
+rm ../../pyconsole/bindings.py
 #::end
 
 #::gen repcsv 'cp ./bindings.py ../../$1/bindings.py' (subdirs)
 cp ./bindings.py ../../server/bindings.py
 cp ./bindings.py ../../manager/bindings.py
 cp ./bindings.py ../../devicemanager/bindings.py
+cp ./bindings.py ../../pyconsole/bindings.py
 #::end
 
