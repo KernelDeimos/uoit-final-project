edc93acc3cb64c228d91590019048f8e78fff180
TSomann <taylorsomann@gmail.com>
2019-04-01 21:31:06 -0400

    Static files

--- Capstone_Winter_Work/HA/server/run.py
+++ Capstone_Winter_Work/HA/server/run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from control_panel.views import control_panel
 
 # TODO: Configure upload folder
@@ -6,6 +6,15 @@ UPLOAD_FOLDER = './packages'
 
 def create_app(config_file):
 	app = Flask(__name__) # Create application object
+
+	@app.route('/js/<path:path>')
+	def send_js(path):
+		return send_from_directory('js', path)
+
+	@app.route('/css/<path:path>')
+	def send_css(path):
+		return send_from_directory('css', path)
+
 	app.config.from_pyfile(config_file) # Configure application
 	app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 	app.register_blueprint(control_panel) # Register control panel
